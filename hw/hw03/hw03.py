a7d856062f8d6553225c394d366c59a377df8e25
ruiyan-ma <maruiyan17@gmail.com>
2022-03-24 00:49:07 -0700

    finish hw03

---
+++ hw/hw03/hw03.py
@@ -0,0 +1,224 @@
+from operator import sub, mul
+from math import pow
+HW_SOURCE_FILE = __file__
+
+
+def composer(func=lambda x: x):
+    """
+    Returns two functions -
+    one holding the composed function so far, and another
+    that can create further composed problems.
+    >>> add_one = lambda x: x + 1
+    >>> mul_two = lambda x: x * 2
+    >>> f, func_adder = composer()
+    >>> f1, func_adder = func_adder(add_one)
+    >>> f1(3) # should be 3 + 1 = 4
+    4
+    >>> f2, func_adder = func_adder(mul_two)
+    >>> f2(3) # should be 1 + (2 * 3) = 7
+    7
+    >>> f3, func_adder = func_adder(add_one)
+    >>> f3(3) # should be 1 + (2 * (3 + 1)) = 9
+    9
+    """
+    def func_adder(g):
+        "*** YOUR CODE HERE ***"
+        # when calling func_adder, python finds func from the parent frame,
+        # which is the composer frame with the previous h functoin as the
+        # argument.
+        def h(x): return func(g(x))
+        return composer(h)
+    return func, func_adder
+
+
+def g(n):
+    """Return the value of G(n), computed recursively.
+
+    >>> g(1)
+    1
+    >>> g(2)
+    2
+    >>> g(3)
+    3
+    >>> g(4)
+    10
+    >>> g(5)
+    22
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'g', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if n <= 3:
+        return n
+    return g(n - 1) + 2 * g(n - 2) + 3 * g(n - 3)
+
+
+def g_iter(n):
+    """Return the value of G(n), computed iteratively.
+
+    >>> g_iter(1)
+    1
+    >>> g_iter(2)
+    2
+    >>> g_iter(3)
+    3
+    >>> g_iter(4)
+    10
+    >>> g_iter(5)
+    22
+    >>> from construct_check import check
+    >>> # ban recursion
+    >>> check(HW_SOURCE_FILE, 'g_iter', ['Recursion'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if n <= 3:
+        return n
+    # 定义动规数组
+    nums = [1, 2, 3]
+    # 确定遍历顺序
+    for i in range(3, n):
+        nums.append(nums[i - 1] + 2 * nums[i - 2] + 3 * nums[i - 3])
+    return nums[-1]
+
+
+def missing_digits(n):
+    """Given a number a that is in sorted, increasing order,
+    return the number of missing digits in n. A missing digit is
+    a number between the first and last digit of a that is not in n.
+    >>> missing_digits(1248) # 3, 5, 6, 7
+    4
+    >>> missing_digits(1122) # No missing numbers
+    0
+    >>> missing_digits(123456) # No missing numbers
+    0
+    >>> missing_digits(3558) # 4, 6, 7
+    3
+    >>> missing_digits(35578) # 4, 6
+    2
+    >>> missing_digits(12456) # 3
+    1
+    >>> missing_digits(16789) # 2, 3, 4, 5
+    4
+    >>> missing_digits(19) # 2, 3, 4, 5, 6, 7, 8
+    7
+    >>> missing_digits(4) # No missing numbers between 4 and 4
+    0
+    >>> from construct_check import check
+    >>> # ban while or for loops
+    >>> check(HW_SOURCE_FILE, 'missing_digits', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    # from the right side to the left side
+    # check two digits each time
+
+    # only one digit left
+    if n < 10:
+        return 0
+
+    # more than one digit
+    rest, last = n // 10, n % 10
+    second_last = rest % 10
+    # compare last and second last
+    if last - second_last <= 1:
+        miss = 0
+    else:
+        miss = last - second_last - 1
+
+    return missing_digits(rest) + miss
+
+
+def count_change(total):
+    """Return the number of ways to make change for total.
+
+    >>> count_change(7)
+    6
+    >>> count_change(10)
+    14
+    >>> count_change(20)
+    60
+    >>> count_change(100)
+    9828
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'count_change', ['While', 'For'])
+    True
+    """
+    def count_partitions(n, m):
+        """Count the ways to partition n using coins greater than 2^m"""
+        coin = pow(2, m)
+
+        if n == 0:
+            # no more coin used, we find a way
+            return 1
+        elif n < coin:
+            # cannot use the current coin, return 0
+            return 0
+        else:
+            # use the current coin, or use greater coins
+            return count_partitions(n - coin, m) + count_partitions(n, m + 1)
+
+    return count_partitions(total, 0)
+
+
+def print_move(origin, destination):
+    """Print instructions to move a disk."""
+    print("Move the top disk from rod", origin, "to rod", destination)
+
+
+def move_stack(n, start, end):
+    """Print the moves required to move n disks on the start pole to the end
+    pole without violating the rules of Towers of Hanoi.
+
+    n -- number of disks
+    start -- a pole position, either 1, 2, or 3
+    end -- a pole position, either 1, 2, or 3
+
+    There are exactly three poles, and start and end must be different. Assume
+    that the start pole has at least n disks of increasing size, and the end
+    pole is either empty or has a top disk larger than the top n start disks.
+
+    >>> move_stack(1, 1, 3)
+    Move the top disk from rod 1 to rod 3
+    >>> move_stack(2, 1, 3)
+    Move the top disk from rod 1 to rod 2
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 2 to rod 3
+    >>> move_stack(3, 1, 3)
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 1 to rod 2
+    Move the top disk from rod 3 to rod 2
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 2 to rod 1
+    Move the top disk from rod 2 to rod 3
+    Move the top disk from rod 1 to rod 3
+    """
+    assert 1 <= start <= 3 and 1 <= end <= 3 and start != end, "Bad start/end"
+    if n == 1:
+        print_move(start, end)
+    else:
+        # poles: 1, 2, 3
+        # find the mid pole
+        mid = 6 - start - end
+        # move n - 1 poles from start to mid
+        move_stack(n - 1, start, mid)
+        # move the nth pole to end
+        print_move(start, end)
+        # move n - 1 poles from mid to end
+        move_stack(n - 1, mid, end)
+
+
+def make_anonymous_factorial():
+    """Return the value of an expression that computes factorial.
+
+    >>> make_anonymous_factorial()(5)
+    120
+    >>> from construct_check import check
+    >>> # ban any assignments or recursion
+    >>> check(HW_SOURCE_FILE, 'make_anonymous_factorial', ['Assign', 'AugAssign', 'FunctionDef', 'Recursion'])
+    True
+    """
+    return 'YOUR_EXPRESSION_HERE'
