8c5a9552b7ad8748994129709ef10f680880cfba
ruiyan-ma <maruiyan17@gmail.com>
2022-05-08 22:29:30 -0700

    finish hw07

--- hw/hw07/hw07.scm
+++ hw/hw07/hw07.scm
@@ -8,7 +8,7 @@
 
 ; return the sign of a number
 (define (sign num)
-  (cond 
+  (cond
     ((< num 0) -1)
     ((= num 0) 0)
     (else      1)))
@@ -19,30 +19,30 @@
 (define (pow x y)
   ; base case: y = 1, just return x
   (if (= y 1)
-      x
-      ; recursive call
-      (if (even? y)
-          (square (pow x (quotient y 2)))
-          (* x (square (pow x (quotient y 2)))))))
+    x
+    ; recursive call
+    (if (even? y)
+      (square (pow x (quotient y 2)))
+      (* x (square (pow x (quotient y 2)))))))
 
 ; remove duplicates in s
 (define (unique s)
   ; base case: s is nil
   (if (null? s)
-      nil
-      ; recursive call
-      (cons (car s)
-            ; remove (car s) in (cdr s), then call unique on the result
-            (unique (filter
-                     (lambda (x) (not (eq? x (car s)))) ; lambda expression
-                     (cdr s))))))
+    nil
+    ; recursive call
+    (cons (car s)
+          ; remove (car s) in (cdr s), then call unique on the result
+          (unique (filter
+                    (lambda (x) (not (eq? x (car s)))) ; lambda expression
+                    (cdr s))))))
 
 ; return a list contains n(number) x
 (define (replicate x n)
   (define (replicate-iter x n lst)
     (if (= n 0)
-        lst
-        (replicate-iter x (- n 1) (cons x lst))))
+      lst
+      (replicate-iter x (- n 1) (cons x lst))))
   (replicate-iter x n nil))
 
 ; combines the first n natural numbers according to the following parameters:
@@ -52,24 +52,25 @@
 ; 4. term: a function of one argument that computes the nth term of a sequence
 (define (accumulate combiner start n term)
   (if (= n 0)
-      start
-      ; combine term(n) and accumulate(n - 1)
-      (combiner (term n)
-                (accumulate combiner start (- n 1) term))))
+    start
+    ; combine term(n) and accumulate(n - 1)
+    (combiner (term n)
+              (accumulate combiner start (- n 1) term))))
 
 ; tail recursion
 (define (accumulate-tail combiner start n term)
   (define (accumulate-iter combiner num term result)
     ; base case: num > n
     (if (> num n)
-        result
-        ; recursive case: combine result and start to get a new result
-        (accumulate-iter combiner
-                         (+ num 1)
-                         term
-                         (combiner result (term num)))))
+      result
+      ; recursive case: combine result and start to get a new result
+      (accumulate-iter combiner
+                       (+ num 1)
+                       term
+                       (combiner result (term num)))))
   (accumulate-iter combiner 1 term start))
 
 (define-macro
- (list-of map-expr for var in lst if filter-expr)
- 'YOUR-CODE-HERE)
+  (list-of map-expr for var in lst if filter-expr)
+  `(map (lambda (,var) ,map-expr)  ; function
+        (filter (lambda (,var) ,filter-expr), lst)))  ; sequence
