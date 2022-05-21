b5719ac8b783e0b75cf9a7c1c578c01a947c558f
ruiyan-ma <maruiyan17@gmail.com>
2022-05-21 13:46:18 -0700

    finish hw08

--- hw/hw08/hw08.scm
+++ hw/hw08/hw08.scm
@@ -1,25 +1,51 @@
 (define (rle s)
-  'YOUR-CODE-HERE
-)
+  ; consume all val element
+  (define (consume s prev count)
+    (if (null? s)
+      ; base case: s is nil, return current result as a stream
+      (cons-stream (list prev count) nil)
+      ; recursive case: consume an element each time
+      (if (eq? prev (car s))
+        ; consume (car s)
+        (consume (cdr-stream s) prev (+ count 1))
+        ; add (prev count) to the stream, then consume next element
+        (cons-stream (list prev count)
+                     (consume s (car s) 0)))))
 
-
+  (if (null? s)
+    nil
+    (consume s (car s) 0)))
 
 (define (group-by-nondecreasing s)
-    'YOUR-CODE-HERE)
+  (define (consume_list s prev group)
+    (if (null? s)
+      ; base case: s is nil, return current group as a stream
+      (cons-stream group nil)
+      ; recursive case: consume a list each time
+      (if (>= (car s) prev)
+        ; consume (car s)
+        (consume_list (cdr-stream s) (car s) (append group (list (car s))))
+        ; add current group to the stream, then consume next element
+        (cons-stream group
+                     (consume_list s (car s) nil)))))
+
+  (if (null? s)
+    nil
+    (consume_list s (car s) nil)))
 
 
 (define finite-test-stream
-    (cons-stream 1
-        (cons-stream 2
-            (cons-stream 3
-                (cons-stream 1
-                    (cons-stream 2
-                        (cons-stream 2
-                            (cons-stream 1 nil))))))))
+  (cons-stream 1
+               (cons-stream 2
+                            (cons-stream 3
+                                         (cons-stream 1
+                                                      (cons-stream 2
+                                                                   (cons-stream 2
+                                                                                (cons-stream 1 nil))))))))
 
 (define infinite-test-stream
-    (cons-stream 1
-        (cons-stream 2
-            (cons-stream 2
-                infinite-test-stream))))
+  (cons-stream 1
+               (cons-stream 2
+                            (cons-stream 2
+                                         infinite-test-stream))))
 
