e19c399b503aa4873b3e285ac7e4f81db8e77e5d
ruiyan-ma <maruiyan17@gmail.com>
2022-05-20 19:32:44 -0700

    finish lab13

--- lab/lab13/lab13.sql
+++ lab/lab13/lab13.sql
@@ -1,33 +1,70 @@
 .read data.sql
-
-
 CREATE TABLE bluedog AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+SELECT color,
+       pet
+FROM students
+WHERE color="blue"
+    AND pet="dog";
+
 
 CREATE TABLE bluedog_songs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+SELECT color,
+       pet,
+       song
+FROM students
+WHERE color="blue"
+    AND pet="dog";
 
 
 CREATE TABLE matchmaker AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+SELECT s1.pet,
+       s1.song,
+       s1.color,
+       s2.color
+FROM students AS s1,
+     students AS s2
+WHERE s1.pet = s2.pet
+    AND s1.song = s2.song
+    AND s1.time < s2.time;
 
 
 CREATE TABLE sevens AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+SELECT stu.seven
+FROM students AS stu,
+     numbers AS num
+WHERE stu.time = num.time
+    AND stu.number = 7
+    AND num."7"="True";
 
 
 CREATE TABLE favpets AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+SELECT pet,
+       count(*) AS number
+FROM students
+GROUP BY pet
+ORDER BY number DESC
+LIMIT 10;
 
 
 CREATE TABLE dog AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+SELECT pet,
+       count(*) AS number
+FROM students
+WHERE pet='dog';
 
 
 CREATE TABLE bluedog_agg AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+SELECT song,
+       count(*) AS number
+FROM bluedog_songs
+GROUP BY song
+ORDER BY number DESC;
 
 
 CREATE TABLE instructor_obedience AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
-
+SELECT seven,
+       instructor,
+       count(*)
+FROM students
+WHERE seven="7"
+GROUP BY instructor;
